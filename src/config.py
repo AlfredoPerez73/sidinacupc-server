edd07c6b472acf2af10e87ea15be861c50f0b4d3
AlfredoPerez73 <apcoco07@gmail.com>
2025-05-05 12:45:19 -0500

    Cambio en la uri

--- src/config.py
+++ src/config.py
@@ -5,5 +5,5 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret'
-    MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb://localhost/sidinacupc'
+    MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb+srv://AlfredoPerez73:Zc5WPwe5ntqfys8x@cluster0.5kxa2yc.mongodb.net/sidinacupc'
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'jwt-secret-key'
